d35b39564e23db37860ca338ebd11d254899cba9
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-12-11 20:08:14 +0800

    v0.0.1

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 module.exports = {
   Test: process.env.Test || false, //Whether testing
   Admins: ['806346991730819121'], //Admins of the bot
-  DefaultPrefix: process.env.Prefix || 'C', //Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || '-', //Default prefix, Server Admins can change the prefix
   Token: process.env.Token || '', //Discord Bot Token
   MongoDB: process.env.MONGODB || '', //MongoDB URL
   EmbedColor: 'RANDOM', //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
