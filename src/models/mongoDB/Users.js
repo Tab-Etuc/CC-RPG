b96250ce98c05653af8378ebb5ddd14435cb0424
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-12-31 20:19:48 +0800

    v0.0.1

--- src/models/mongoDB/Users.js
+++ src/models/mongoDB/Users.js
@@ -6,15 +6,18 @@ const usersSchema = mongoose.Schema({
   陣營: { type: String, required: false },
   種族: { type: String, required: false },
   裝備: { type: Array, required: false, default: [] },
-  背包: { type: Object, required: false },
+  背包: { type: Array, required: false, default: [] },
   等級: { type: Number, required: false, default: 1 },
   評級: { type: String, required: false, default: 'E' },
   經驗值: { type: Number, required: false, default: 1 },
   升等所需經驗值: { type: Number, required: false, default: 100 },
   金錢: { type: Number, required: false, default: 0 },
   技能: { type: Array, required: false, default: ['數學者'] },
+  TMP: { type: Number, required: false, default: 100 }, // 總共的魔力值
+  MP: { type: Number, required: false, default: 100 }, // 魔力值
+  THP: { type: Number, required: false, default: 100 }, //總共的血量
+  HP: { type: Number, required: false, default: 100 }, // 血量
   屬性: { type: Object, required: false },
-  升等增加之屬性: { type: Object, required: false },
   事件紀錄: {
     type: Object,
     required: false,
