49d02fb18c78d057c6c3bb588935f3378e891b10
Tab-Etuc <xji.gl4.vu@gmail.com>
2022-01-01 18:56:03 +0800

    v0.0.1

---
+++ src/commands/help.js
@@ -0,0 +1,43 @@
+const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js')
+const fs = require('fs')
+
+module.exports = {
+  name: 'help',
+  description: '幫助頁面',
+  usage: '',
+  permissions: {
+    channel: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS'],
+    member: []
+  },
+  aliases: ['h'],
+  /**
+   *
+   * @param {import("../base/CC-OSV-Client")} bot
+   * @param {import("discord.js").Message} message
+   * @param {string[]} args
+   * @param {*} param3
+   */
+
+  run: async (bot, message, args, GuildDB) => {
+    let Fields = []
+    let Field = {}
+    const commandFiles = fs
+      .readdirSync('./src/commands')
+      .filter(file => file.endsWith('.js'))
+
+    for (const file of commandFiles) {
+      const command = require(`./${file}`)
+      Field.name = command.name
+      Field.value = `\`${command.description}\``
+      Field.inline = true
+      Fields.push(Field)
+      console.log(Fields)
+      Field = {}
+    }
+    message.reply({
+      embeds: [
+        bot.say.msgInfo(`\`\`\`md\n# 指令列表\n\`\`\``).setFields(Fields)
+      ]
+    })
+  }
+}
---
+++ src/commands/mission.js
@@ -0,0 +1,25 @@
+const { MessageButton, MessageActionRow } = require('discord.js')
+const Users = require('../models/mongoDB/Users.js')
+
+const Bar = require('../models/Bar')
+module.exports = {
+  name: 'mission',
+  description: '神使任務',
+  usage: '',
+  permissions: {
+    channel: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS'],
+    member: []
+  },
+  aliases: ['m'],
+  /**
+   *
+   * @param {import("../base/CC-OSV-Client")} bot
+   * @param {import("discord.js").Message} message
+   * @param {string[]} args
+   * @param {*} param3
+   */
+
+  run: async (bot, message, args, GuildDB) => {
+    
+  }
+}
