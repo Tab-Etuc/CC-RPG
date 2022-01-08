6b856acf891c28ca71058466f891b80da788f7ff
Tab-Etuc <xji.gl4.vu@gmail.com>
2022-01-08 20:09:04 +0800

    v0.0.1

---
+++ src/assets/Data/skill.js
@@ -0,0 +1,15 @@
+const array = [
+  {
+    name: '數學者',
+    class: 1,
+    description:
+      '「數學者」將使血量、魔力等具現數字化，並將事件執行的成功機率計算後轉換為實質數字呈現於您眼前。',
+    run: async (bot, message, args) => {}
+  },
+  {
+    name: '賢者',
+    class: 2,
+    description: '「賢者」為「數學者」的上位技能。得以更加強勢地解析、計算。',
+    run: async (bot, message, args) => {}
+  }
+]
